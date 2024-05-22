d9733726b6349945205480f468e4a19a873576e5
Irxsvaa <163717598+Irxsvaa@users.noreply.github.com>
2024-05-22 14:38:47 -0700

    js

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleDescription(className) {
+    var desH = document.getElementsByClassName(className)[0];
+    if (desH.style.display === "none") {
+        desH.style.display = "block";
+    } else {
+        desH.style.display = "none";
+    }
+}
+
+
