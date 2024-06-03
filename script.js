c40599cf307b622b7f37dd92ec22c2a2e5ed49b9
Irxsvaa <163717598+Irxsvaa@users.noreply.github.com>
2024-06-03 14:34:47 -0700

    ,,

--- script.js
+++ script.js
@@ -9,24 +9,29 @@ function toggleDescription(className) {
     }
 }
 
-const prev = document.getElementById('prev');
-let carousel = document.querySelector('.carousel');
-let items = document.querySelectorAll('.item');
-let index = 0;
+
+
+
 
 
 window.addEventListener("DOMContentLoaded", (event) => {
     const next = document.getElementById('next');
-
+    const prev = document.getElementById('prev');
+    const carousel = document.querySelector('.carousel');
+    const items = document.querySelectorAll('.item');
+    let index = 0;
     if (next) {
         next.addEventListener('click', function() {
             index = (index + 1) % items.length;
             carousel.style.transform = `translateX(${-index * 100}%)`;
         });
     }
+
+    if (next) {
+        prev.addEventListener('click', function() {
+            index = (index - 1 + items.length) % items.length;
+            carousel.style.transform = `translateX(${-index * 100}%)`;
+        });
+    }
 });
 
-prev.addEventListener('click', function() {
-    index = (index - 1 + items.length) % items.length;
-    carousel.style.transform = `translateX(${-index * 100}%)`;
-});
